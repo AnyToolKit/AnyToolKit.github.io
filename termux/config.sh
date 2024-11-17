5ef08c45e6ec473ae54b3d1cba531cc2f2712e1b
hexgio <hexgio@outlook.com>
2024-11-17 14:21:37 +0800

    test

--- termux/config.sh
+++ termux/config.sh
@@ -1,16 +1,18 @@
-case base in
-	# 更新与升级
-	pkg update
-	pkg upgrade
+case config in
+	*)
+		# 更新与升级
+		pkg update
+		pkg upgrade
 
-	# 安装欢迎界面列出来的软件
-	pkg install -y root-repo x11-repo 
+		# 安装欢迎界面列出来的软件
+		pkg install -y root-repo x11-repo 
 
-	# 安装基本的编辑工具
-	pkg install -y vim
+		# 安装基本的编辑工具
+		pkg install -y vim
 
-	# 安装常用工具
-	pkg install -y net-tools git wget curl
+		# 安装常用工具
+		pkg install -y net-tools git wget curl
 
-	pkg install -y openssh
+		pkg install -y openssh
+		;;
 esac
