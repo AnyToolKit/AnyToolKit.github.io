15ce3cf0e8790d203e292a907b60bf403c1b16bf
hexgio <hexgio@outlook.com>
2024-11-17 16:18:23 +0800

    test

--- termux/config.sh
+++ termux/config.sh
@@ -12,7 +12,7 @@ case $1 in
 		pkg install -y vim emacs
 
 		# 安装常用工具
-		pkg install -y net-tools git wget curl tree
+		pkg install -y net-tools git wget curl tree screen
 
 		pkg install -y openssh
 		;;	
