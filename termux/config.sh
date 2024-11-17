cd159c72712e6cf35898030dde678c2f8b869bd1
hexgio <hexgio@outlook.com>
2024-11-17 14:42:37 +0800

    test

--- termux/config.sh
+++ termux/config.sh
@@ -15,8 +15,17 @@ case $1 in
 
 		pkg install -y openssh
 		;;
+	CS)
+		sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+
+		sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+
+		sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+
+		pkg update
+		;;
 	*)							# 默认显示用户信息
 		# echo "用法：$0 {start|stop|status|restart}"
-		echo "用法：$0 {base}"
+		echo "用法：$0 {base|CS}"
 		;;
 esac
