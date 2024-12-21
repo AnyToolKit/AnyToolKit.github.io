366e6eff42458d97b613316531ced49957c9398a
hexgio <hexgio@outlook.com>
2024-12-21 15:19:46 +0800

    test

--- termux/dvwa_config.sh
+++ termux/dvwa_config.sh
@@ -2,3 +2,5 @@ pkg install -y mariadb
 cd $PREFIX/etc/
 mkdir -p my.cnf.d
 mysql_install_db				# 初始化数据库
+nohup mysqld &					# 启动 MySQL 服务
+# ps aux | grep -v "grep" | grep "mysql" | awk '{print $2}' | xargs kill -9 # 关闭数据库
